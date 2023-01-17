635040aac7d604430a2304349964baa5354c79e1
Muhammadyahyo <muhammadyahyodev@gmail.com>
2023-01-17 13:45:14 +0500

    finish project

--- src/app.module.ts
+++ src/app.module.ts
@@ -20,6 +20,22 @@ import { UserModule } from './user/user.module';
 import { UserAddressModule } from './user-address/user-address.module';
 import { MailModule } from './mail/mail.module';
 import { FilesModule } from './files/files.module';
+import { Shop } from './shop/schemas/shop.model';
+import { Jewelry } from './jewelry/schemas/jewelry.model';
+import { Brand } from './brand/schemas/brand.model';
+import { UserAddress } from './user-address/schemas/user-address.model';
+import { UserRole } from './user-roles/schemas/user-role.model';
+import { District } from './district/schemas/district.model';
+import { Booking } from './booking/schemas/booking.model';
+import { PaymentMethod } from './payment_method/schemas/payment_method.model';
+import { Cart } from './cart/schemas/cart.model';
+import { Review } from './review/schemas/review.model';
+import { Region } from './region/schemas/region.model';
+import { User } from './user/schemas/user.model';
+import { Status } from './status/schemas/status.model';
+import { Detail } from './detail/schemas/detail.model';
+import { Material } from './material/schemas/material.model';
+import { JewelryType } from './jewelry-type/schemas/jewelry-type.model';
 
 @Module({
   imports: [
@@ -31,10 +47,27 @@ import { FilesModule } from './files/files.module';
       dialect: 'postgres',
       host: process.env.POSTGRES_HOST,
       port: Number(process.env.POSTGRES_PORT),
-      username: process.env.POSTGRES_USER,
-      password: process.env.POSTGRES_PASSWORD,
-      database: process.env.POSTGRES_DB,
-      models: [],
+      username: String(process.env.POSTGRES_USER),
+      password: String(process.env.POSTGRES_PASSWORD),
+      database: String(process.env.POSTGRES_DB),
+      models: [
+        Shop,
+        Jewelry,
+        Brand,
+        UserAddress,
+        UserRole,
+        District,
+        Booking,
+        PaymentMethod,
+        Cart,
+        Review,
+        Region,
+        User,
+        Status,
+        Detail,
+        Material,
+        JewelryType,
+      ],
       autoLoadModels: true,
       logging: false,
     }),
